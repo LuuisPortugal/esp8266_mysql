aa6c05a460722815e81f9a56be3953102fb78deb
Luis Enrique Portugal <luis.portugal@sofintech.com.br>
2021-10-22 12:38:55 -0300

    database

--- database/db.sql
+++ database/db.sql
@@ -1,20 +1,33 @@
 create database esp8266_mysql;
 use esp8266_mysql;
 
-create table pinos
+create table if not exists pinos
 (
-    id int,
-    pino varchar(2) not null,
-    horario timestamp not null,
-    estado varchar(255) null
+	id int auto_increment,
+	pino varchar(3) not null,
+	descricao text null,
+	tipo varchar(10) not null,
+	constraint pinos_id_uindex
+		unique (id),
+	constraint pinos_pino_uindex
+		unique (pino)
 );
 
-create unique index pinos_id_uindex
-    on pinos (id);
-
 alter table pinos
-    add constraint pinos_pk
-        primary key (id);
+	add primary key (id);
 
-alter table pinos modify id int auto_increment;
+create table if not exists pinos_valores
+(
+	id int auto_increment,
+	pino varchar(3) not null,
+	horario timestamp not null,
+	valor varchar(255) null,
+	constraint pinos_valores_id_uindex
+		unique (id)
+);
+
+alter table pinos_valores
+	add primary key (id);
+
+grant alter on table pinos_valores to root;
 
