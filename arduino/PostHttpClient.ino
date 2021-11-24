62d40172563915b1c894d1e70c5db4c64dabce1f
LuuisPortugal <luisenriquegomesportugal@gmail.com>
2021-11-24 16:01:16 +0000

    Last changes

--- arduino/PostHttpClient.ino
+++ arduino/PostHttpClient.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#define SERVER_IP "ec2-18-191-253-24.us-east-2.compute.amazonaws.com:3000"
+#define SERVER_IP "ec2-52-14-34-76.us-east-2.compute.amazonaws.com:3000"
 
 #ifndef STASSID
 #define STASSID "NOME_DO_WIFI"
